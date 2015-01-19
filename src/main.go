fc44d31fe62bbde59cdf7274e246310caee6fa78
Hamcha <zikyky@gmail.com>
2015-01-19 15:18:31 +0100

    Basic provider loops and ip fetching

--- src/main.go
+++ src/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"strings"
-	"encoding/json"
-	"io/ioutil"
-	"time"
 	"./providers"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
 )
 
 type Provider interface {
-	Update(record string, ip string)
+	Update(record string, ip string) (error, bool)
 }
 
 type ProviderConfig struct {
@@ -20,7 +21,7 @@ type ProviderConfig struct {
 }
 
 type Config struct {
-	Interval int
+	Interval  time.Duration
 	Providers map[string]ProviderConfig
 }
 
@@ -36,16 +37,79 @@ func main() {
 		panic(err.Error())
 	}
 
-	providers := make([]Provider, 0)
+	providerList := make(map[string]Provider)
 	for key, _ := range config.Providers {
 		switch strings.ToLower(key) {
 		case "gandi":
-			apikey, ok := config.Providers[key].ProviderInfo["Apikey"]
+			apikey, ok := config.Providers[key].ProviderInfo["Apikey"].(string)
 			if ok {
-			providers.Gandi(apikey)
+				gandi, err := providers.Gandi(apikey, config.Providers[key].Domain)
+				if err != nil {
+					fmt.Println(now() + " [Gandi] Failed: " + err.Error())
+				} else {
+					providerList["Gandi"] = gandi
+				}
+			} else {
+				fmt.Println(now() + " [Gandi] API key missing")
 			}
 			break
 		}
 	}
-	switch stirngs.ToLower()
+
+	oldip := getIp()
+	queue := make(map[string]Provider, 0)
+	for {
+		// If no providers are left exit
+		if len(providerList) < 1 {
+			fmt.Println(now() + " [-] No providers in list, exiting..")
+			return
+		}
+
+		ip := getIp()
+
+		if ip != oldip {
+			for key, provider := range providerList {
+				queue[key] = provider
+			}
+			oldip = ip
+		}
+
+		if len(queue) > 0 {
+			for key, provider := range queue {
+				err, fatal := provider.Update(config.Providers[key].Record, ip)
+				if err != nil {
+					if fatal {
+						fmt.Println(now() + " [" + key + "] ERROR - " + err.Error())
+					} else {
+						fmt.Println(now() + " [" + key + "] FATAL - " + err.Error())
+						delete(providerList, key)
+						delete(queue, key)
+					}
+				} else {
+					fmt.Println(now() + " [" + key + "] Updated")
+					delete(queue, key)
+				}
+			}
+		}
+
+		// Wait next interval
+		time.Sleep(config.Interval * time.Second)
+	}
+}
+
+func now() string {
+	return time.Now().Format(time.Stamp)
+}
+
+func getIp() string {
+	resp, err := http.Get("https://myexternalip.com/raw")
+	if err != nil {
+		fmt.Println(now() + " [-] Can't get IP address!")
+		panic(err.Error())
+	}
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+	return string(bytes)
 }
--- src/providers/gandi.go
+++ src/providers/gandi.go
@@ -5,11 +5,19 @@ const gandiURL = "https://rpc.ote.gandi.net/xmlrpc/"
 
 type GandiProvider struct {
 	ApiKey string
+	Domain string
 	OTE    bool
 }
 
-func Gandi(api string) GandiProvider {
+func Gandi(api, domain string) (GandiProvider, error) {
 	var gandi GandiProvider
 	gandi.ApiKey = api
-	return gandi
+	gandi.Domain = domain
+	gandi.OTE = true
+	return gandi, nil
+}
+
+func (g GandiProvider) Update(record string, ip string) (error, bool) {
+
+	return nil, false
 }
