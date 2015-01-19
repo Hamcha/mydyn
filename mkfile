f458d5e398fbf87d2dd0f5ed22ae337799913489
Hamcha <zikyky@gmail.com>
2015-01-19 18:47:50 +0100

    Changed XMLRPC lib

--- mkfile
+++ mkfile
@@ -1,9 +1,14 @@
+all: mydyn
+
 mydyn: deps
 	go build -o mydyn ./src
 
 deps:
-	go get github.com/kolo/xmlrpc
+	go get github.com/divan/gorilla-xmlrpc/xml
 	touch deps
 
-clean:
-	rm -f deps mydyn
+clean: go-clean
+	rm -f deps
+
+go-clean:
+	rm -f mydyn
--- src/providers/gandi.go
+++ src/providers/gandi.go
@@ -1,12 +1,23 @@
 // Gandi.net API (XML-RPC) provider
 package providers
 
-const gandiURL = "https://rpc.ote.gandi.net/xmlrpc/"
+import (
+	"bytes"
+	"fmt"
+	"github.com/divan/gorilla-xmlrpc/xml"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+const gandiURL = "https://rpc.gandi.net/xmlrpc/"
+const gandiOTEURL = "https://rpc.ote.gandi.net/xmlrpc/"
 
 type GandiProvider struct {
 	ApiKey string
 	Domain string
 	OTE    bool
+	URL    string
 }
 
 func Gandi(api, domain string) (GandiProvider, error) {
@@ -14,10 +25,42 @@ func Gandi(api, domain string) (GandiProvider, error) {
 	gandi.ApiKey = api
 	gandi.Domain = domain
 	gandi.OTE = true
-	return gandi, nil
+	gandi.URL = gandiURL
+	if gandi.OTE {
+		gandi.URL = gandiOTEURL
+	}
+
+	err := gandi.Auth()
+	return gandi, err
+}
+
+func (g GandiProvider) Auth() error {
+	req := struct {
+		Apikey string
+	}{
+		g.ApiKey,
+	}
+	buf, _ := xml.EncodeClientRequest("domain.tld.list", &req)
+	fmt.Println(string(buf))
+	reply, err := post(g.URL, buf)
+	if err != nil {
+		return err
+	}
+	data, err := ioutil.ReadAll(reply)
+	fmt.Println(data)
+	return err
 }
 
 func (g GandiProvider) Update(record string, ip string) (error, bool) {
 
 	return nil, false
+}
+
+func post(url string, request []byte) (io.ReadCloser, error) {
+	resp, err := http.Post(url, "text/xml", bytes.NewBuffer(request))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return resp.Body, err
 }
